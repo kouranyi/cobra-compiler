d4a5ab9248fb863b7b71cdebdf699d97147896f8
ikokorany <ikokorany@gmail.com>
2019-01-04 03:23:14 +0200

    adding TokenType

--- src/TokenType.java
+++ src/TokenType.java
@@ -1,4 +1,4 @@
-public enum TokenType {
+public  enum TokenType {
     String,Bool,Int,Enter,Print,Execute,If,From,To,Step,Eof,
     LeftParen,RightParen,Assign,Less,LessEqual,Greater,GreaterEqual,
     Plus,Minus,Multiply,Divide,Identifier,IntLiteral,CharLiteral
